b8956055f9a94bbb85dce9a211a5539f68a65c27
André Sartori <49533777+arsartori@users.noreply.github.com>
2024-08-03 13:43:42 -0300

    Atualizado

--- start-znuny.sh
+++ start-znuny.sh
@@ -6,11 +6,6 @@
 if [ $ZNUNY_UPDATE == 'yes' ]; then
         echo "Opção Update selecionada. Atualizando..."
 	su -c "/opt/otrs/scripts/DBUpdate-to-6.pl" -s /bin/bash otrs
-elif [ $ZNUNY_UPGRADE == 'yes' ]; then
-        echo "Opção Upgrade selecionada. Atualizando..."
-	su -c "/opt/otrs/scripts/MigrateToZnuny6_1.pl" -s /bin/bash otrs
-else
-	echo "Nenhuma opção selecionada"
 fi
 # Start cron
 su -c "/opt/otrs/bin/Cron.sh start" -s /bin/bash otrs
